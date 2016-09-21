5d1d6350e4371063841dc5c3fa6aace7bd563ed5
Slava Chernoy <vchernoy@gmail.com>
2016-09-21 01:04:55 -0700

    implement expand

--- calc.py
+++ calc.py
@@ -115,6 +115,23 @@ class Reader:
     def expected_next(self, expected_vals):
         return self.has_next() and (self.look_next() in expected_vals)
 
+
+class TokenReader:
+    def __init__(self, source):
+        self.src = source
+        self.loc = -1
+
+    def look_next(self):
+        if self.loc + 1 >= len(self.src):
+            raise Exception("no input")
+
+        return self.src[self.loc+1]
+
+    def move_next(self):
+        tok = self.look_next()
+        self.loc += 1
+        return tok
+
 class Node:
     num = 'num.'
     var = 'var'
@@ -165,6 +182,9 @@ class Node:
         if self.operation in [Node.neg, Node.inv]:
             assert type(operands) != list
 
+    def __str__(self):
+        return self.to_str()
+
     def to_str(self, in_parenthesis = False):
         if self.operation == Node.var:
             res = self.operands
@@ -182,11 +202,11 @@ class Node:
             # error
             pass
 
-        if res and in_parenthesis and self.operation in [Node.add, Node.neg, Node.mul, Node.inv]:
-            res = '(' + res + ')'
+        if in_parenthesis:
+            if self.operation in [Node.add, Node.neg, Node.mul, Node.inv] or ((self.operation == Node.num) and (self.operands < 0)):
+                res = '(' + res + ')'
 
         return res
-
 
     def simplify(self):
         if self.operation == Node.var:
@@ -202,10 +222,41 @@ class Node:
                 else:
                     evaluated.append(evaluated_operand)
 
-            numeric = [n.operands for n in evaluated if n.operation == Node.num]
-            symbolic = [n for n in evaluated if n.operation != Node.num]
+            simple = []
+            complex = []
+            for n in evaluated:
+                if n.as_term(False) == None:
+                    complex.append(n)
+                else:
+                    simple.append(n)
 
-            val = sum(numeric) if numeric else 0
+            terms = Node.addition(simple).as_term()
+            simple = []
+            for t in terms:
+                simple.append(Node.multiplication([Node.number(t[0])] + [Node.variable(var) for var in t[1]]))
+
+            evaluated = [Node.addition(simple)] + complex
+
+            neg_degree = []
+            pos_degree = {}
+            val = 0
+            for n in evaluated:
+                if n.operation == Node.num:
+                    val += n.operands
+                elif n.degree() < 0:
+                    neg_degree.append(n)
+                else:
+                    pos_degree.setdefault(n.degree(), []).append(n)
+
+
+            symbolic = []
+            x = [(d, pos_degree[d]) for d in pos_degree]
+            x.sort()
+            for d,l in x:
+                symbolic = symbolic + l
+
+            symbolic += neg_degree
+
 
             if symbolic and (val == 0):
                 res = Node.addition(symbolic)
@@ -234,12 +285,19 @@ class Node:
                 else:
                     evaluated.append(evaluated_operand)
 
-            numeric = [n.operands for n in evaluated if n.operation == Node.num]
-            symbolic = [n for n in evaluated if n.operation != Node.num]
-
             val = 1
-            for v in numeric:
-                val *= v
+            vars = []
+            others = []
+            for n in evaluated:
+                if n.operation == Node.num:
+                    val *= n.operands
+                elif n.operation == Node.var:
+                    vars.append((n.operands, n))
+                else:
+                    others.append(n)
+
+            vars.sort()
+            symbolic = [v[1] for v in vars] + others
 
             if symbolic and (val == 1):
                 res = Node.multiplication(symbolic)
@@ -255,9 +313,49 @@ class Node:
             elif (evaluated.operation == Node.inv) and len(evaluated.vars()) == 0:
                 res = evaluated.operands
             else:
-                res = Node.negative(evaluated)
+                res = Node.inverse(evaluated)
 
         return res
+
+
+    def expand(self):
+        if self.operation == Node.var:
+            res = self
+        elif self.operation == Node.num:
+            res = self
+        elif self.operation == Node.add:
+            res = Node.addition([n.expand().simplify() for n in self.operands])
+        elif self.operation == Node.neg:
+            if self.operands.operation == Node.add:
+                res = Node.addition([Node.negative(n).simplify().expand() for n in self.operands.operands])
+            else:
+                res = Node.negative(self.operands.expand()).simplify()
+        elif self.operation == Node.mul:
+            expanded = []
+            for n in self.operands:
+                operand = n.simplify().expand().simplify()
+                expanded.append(operand)
+
+            simple = []
+            complex = []
+            for n in expanded:
+                if n.as_term() == None:
+                    complex.append(n)
+                else:
+                    simple.append(n)
+
+            terms = Node.multiplication(simple).as_term()
+            simple = []
+            for t in terms:
+                simple.append(Node.multiplication([Node.number(t[0])] + [Node.variable(var) for var in t[1]]))
+
+            return Node.multiplication([Node.addition(simple)] + complex).simplify()
+
+        elif self.operation == Node.inv:
+            res = self
+
+        return res
+
 
     def vars(self):
         if self.operation == Node.num:
@@ -274,6 +372,115 @@ class Node:
             res.update(n.vars())
 
         return res
+
+    def degree(self, var=None):
+        if self.operation == Node.num:
+            return 0
+
+        if self.operation == Node.var:
+            return 1 if not var or (var == self.operands) else 0
+
+        if self.operation == Node.neg:
+            return self.operands.degree(var)
+
+        if self.operation == Node.inv:
+            return -self.operands.degree(var)
+
+        if self.operation == Node.add:
+            return max([n.degree(var) for n in self.operands])
+
+        if self.operation == Node.mul:
+            return sum([n.degree(var) for n in self.operands])
+
+    def as_term(self, expand=True):
+        if self.operation == Node.num:
+            return [(self.operands, [])]
+
+        if self.operation == Node.var:
+            return [(1, [self.operands])]
+
+        if self.operation == Node.neg:
+            terms = self.operands.as_term(expand)
+            if terms == None:
+                return None
+
+            return [(-t[0], t[1]) for t in terms]
+
+        if self.operation == Node.inv:
+            terms = self.operands.as_term(expand)
+            if not terms or (len(terms) > 1) or terms[0][1]:
+                return None
+
+            return [(1./terms[0][0], [])]
+
+        if self.operation == Node.add:
+            terms = []
+            for n in self.operands:
+                t = n.as_term(expand)
+                if t == None:
+                    return None
+
+                terms = terms + t
+
+            str_terms = {}
+            for t in terms:
+                s = ','.join(t[1])
+                if s in str_terms:
+                    str_terms[s] = (str_terms[s][0] + t[0], t[1])
+                else:
+                    str_terms[s] = t
+
+                if str_terms[s][0] == 0:
+                    del str_terms[s]
+
+            return str_terms.values() if str_terms else [(0, [])]
+
+        if self.operation == Node.mul:
+            terms_list = []
+            for n in self.operands:
+                terms = n.as_term(expand)
+                if terms == None:
+                    return None
+
+                terms_list.append(terms)
+
+            # 2x 4y = 8x y
+            # 2x (z + 3y) = 2x*z + 6x*y
+            # 2x (z + 3y) (2a + b) =
+            def expand_tems(terms_list, expand):
+                if not terms_list:
+                    return []
+
+                res_terms = [(1,[])]
+                for terms in terms_list:
+                    expanded_terms = []
+                    if expand or ((len(res_terms) == 1) and (len(terms) == 1)):
+                        for t1 in res_terms:
+                            for t2 in terms:
+                                expanded_terms.append((t1[0]*t2[0], sorted(t1[1]+t2[1])))
+                    else:
+                        return None
+
+                    res_terms = expanded_terms
+
+                return res_terms
+
+            terms = expand_tems(terms_list, expand)
+            if terms == None:
+                return None
+
+            str_terms = {}
+            for t in terms:
+                s = ','.join(t[1])
+                if s in str_terms:
+                    str_terms[s] = (str_terms[s][0] + t[0], t[1])
+                else:
+                    str_terms[s] = t
+
+                if str_terms[s][0] == 0:
+                    del str_terms[s]
+
+            return str_terms.values() if str_terms else [(0, [])]
 
 class Error:
     def __init__(self, loc, msg):
@@ -294,7 +501,7 @@ class Parser:
         # EE := E [= E]
 
         tree = self.parse_expr(reader, errors)
-        if reader.has_next() and (reader.look_next().typ == Token.equal):
+        if reader.look_next().typ == Token.equal:
             reader.move_next()
             tree2 = self.parse_expr(reader, errors)
             if reader.look_next().typ != Token.eol:
@@ -324,7 +531,7 @@ class Parser:
             tree = Node.negative(tree)
 
         operands = [tree]
-        while reader.has_next() and (reader.look_next().typ in [Token.add, Token.sub]):
+        while reader.look_next().typ in [Token.add, Token.sub]:
             neg = reader.look_next().typ == Token.sub
             reader.move_next()
             tree = self.parse_product(reader, errors)
@@ -343,17 +550,16 @@ class Parser:
         operation = None
         operands = []
         while True:
-            tok = reader.look_next()
-            if tok.typ == Token.id:
-                tree = Node.variable(tok.lit)
+            if reader.look_next().typ == Token.id:
+                tree = Node.variable(reader.look_next().lit)
                 reader.move_next()
-            elif tok.typ == Token.number:
-                tree = Node.number(tok.number)
+            elif reader.look_next().typ == Token.number:
+                tree = Node.number(reader.look_next().number)
                 reader.move_next()
-            elif tok.typ == Token.l_paren:
+            elif reader.look_next().typ == Token.l_paren:
                 tree = self.parse_expr_in_parenthesis(reader, errors)
             else:
-                errors.append(Error(tok.loc, 'expected P-token, received token \'{}\''.format(tok)))
+                errors.append(Error(reader.look_next().loc, 'expected P-token, received token \'{}\''.format(reader.look_next())))
                 return None
 
             if operation == Node.mul:
@@ -363,13 +569,13 @@ class Parser:
             else:
                 operands.append(tree)
 
-            if reader.has_next() and (reader.look_next().typ == Token.mul):
+            if reader.look_next().typ == Token.mul:
                 operation = Node.mul
                 reader.move_next()
-            elif reader.has_next() and (reader.look_next().typ == Token.div):
+            elif reader.look_next().typ == Token.div:
                 operation = Node.inv
                 reader.move_next()
-            elif reader.has_next() and (tree.operation == Node.num) and (reader.look_next().typ in [Token.id, Token.l_paren]) and (operation in [None, Node.mul]):
+            elif (tree.operation == Node.num) and (reader.look_next().typ in [Token.id, Token.l_paren]) and (operation in [None, Node.mul]):
                 operation = Node.mul
             else:
                 break
@@ -392,6 +598,13 @@ class Parser:
         reader.move_next()
         return tree
 
+
+def parse(s, errors):
+    toks = Tokenizer().tokenize(Reader(s))
+    tok_reader = TokenReader([t for t in toks])
+    return Parser().parse(tok_reader, errors)
+
+
 tokenizer = Tokenizer()
 # reader = Reader('(-2 + 3.5 + x + abc) - 2 - 4')
 # reader = Reader('2 * 3 + 5x*(2+3x) + 5(2+3)/4x + 2/3')
@@ -405,16 +618,19 @@ input = '-2 * 3(10-5) / 2 *3r=2r/r + 2/(1/(10/(1/10))) + 2/(1/x) + (-(-(-(-x))))
 # reader = Reader('2/(1/x)')
 # reader = Reader('1/(2/x)')
 input = '10(1+5)-34*20/34/2*3x=10(1+x)'
+input = '-3(x+1)*(2x-5)*(-x-2)+6x'
+input = 'x*x*x*z + 5y*x - 2 - x * (b*b+5a) - ((-2)+(5*x*y)+((-5)*a*x)+(-(b*x*b))+(x*z*x*x))'
+# input = '(a + b) * (a + b)'
+# input = 'x * (1+a)'
 
-reader = Reader(input)
-toks = tokenizer.tokenize(reader)
-
-tok_reader = Reader([t for t in toks])
+# input = 'x * (b*b+5a - 2*b*b)'
+toks = tokenizer.tokenize(Reader(input))
+tok_reader = TokenReader([t for t in toks])
 parser = Parser()
 errors = []
+
 ast = parser.parse(tok_reader, errors)
-# for t in toks:
-#     print t,
+
 if errors:
     for err in errors:
         print err
@@ -423,10 +639,63 @@ if errors:
         print ' ' * err_loc + '^'
 
 if ast:
-    print ast.to_str()
+    print input
 
-    print ast.simplify().to_str()
-    print ast.simplify().simplify().to_str()
+    print ast
+    print ast.degree()
+    print ast.degree('x')
+
+    print ast.simplify()
+    print ast.simplify().simplify()
 
     print ast.vars()
     print ast.simplify().vars()
+
+    print ast.expand()
+    print ast.expand().expand()
+
+    print ast.expand().simplify()
+
+    print 'ast'
+    t = ast
+    print t
+    print t.as_term()
+    print t.as_term(False)
+    print
+    print 'ast.simplify()'
+    t = ast.simplify()
+    print t
+    print t.as_term()
+    print t.as_term(False)
+    print
+    print 'ast.expand()'
+    t = ast.expand()
+    print t
+    print t.as_term()
+    print t.as_term(False)
+    print
+    print 'ast.expand().simplify()'
+    t = ast.expand().simplify()
+    print t
+    print t.as_term()
+    print t.as_term(False)
+    print
+
+    print 'ast.simplify().expand()'
+    t = ast.simplify().expand()
+    print t
+    print t.as_term()
+    print t.as_term(False)
+    print
+
+    print 'ast.simplify().expand().simplify()'
+    t = ast.simplify().expand().simplify()
+    print t
+    print t.as_term()
+    print t.as_term(False)
+    print
+
+    print ast.simplify().expand().simplify()
+    print ast.simplify().expand().expand().simplify()
+    print parse(str(ast.simplify().expand().expand().simplify()), errors).simplify()
+    print errors
