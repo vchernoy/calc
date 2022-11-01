8441f131ac84aa220ab801e6b3e3328259f2b78a
Slava Chernoy <vchernoy@gmail.com>
2022-11-01 18:07:52 +0000

    parser.py edited online with Bitbucket

--- symexpr/parser.py
+++ symexpr/parser.py
@@ -1,17 +1,180 @@
-import symexpr.tokenizer as tokenizer
-import symexpr.ast as ast
+import enum
+import string
 
 
-class TokenReader:
+class Type(enum.Enum):
     """
-    Provides to the next token from the input.
+    Defines the types of tokens that are created by tokenizer as result of processing the input string.
+    When tokenizer (Lexer) reaches the end of the input, it generates eol-token.
+    When it finds an invalid character, it creates an err-token.
+    """
+    l_paren = '('
+    r_paren = ')'
+    number = 'num'
+    id = 'id'
+    add = '+'
+    sub = '-'
+    mul = '*'
+    div = '/'
+    equal = '='
+    error = 'err'
+    eol = 'EOL'
+
+
+simple_tokens = {t.value: t for t in [
+    Type.l_paren,
+    Type.r_paren,
+    Type.add,
+    Type.sub,
+    Type.mul,
+    Type.div,
+    Type.equal,
+]}
+
+
+class Error:
+    def __init__(self, loc, expected_chars, parsed_chars, next_char):
+        assert type(loc) == int
+
+        self.location = loc
+        self.expected_chars = expected_chars
+        self.parsed_chars = parsed_chars
+        self.next_char = next_char
+
+    def __str__(self) -> str:
+        return f'Error @ {self.location}: expected={self.expected_chars}, parsed={self.parsed_chars}, next={self.next_char}'
+
+
+class Token:
+    """
+    Each token contains its type, see above, and location -- the starting position in the input.
+    If typ is Type.id, the token contains name (of variable or 'log')
+    If typ is Type.number, the token contains number (integer of float)
+    If type is Type.error, the token contains err representing the Lexer error.
+    """
+    def __init__(self, loc, typ, name=None, num=None, err=None):
+        assert type(typ) == Type
+        assert type(loc) == int
+
+        self.location = loc
+        self.typ = typ
+        self.name = name
+        self.number = num
+        self.err = err
+
+    def __repr__(self) -> str:
+        return f'({";".join(str(t) for t in [self.typ, self.name, self.number, self.err])}:{self.location})'
+
+    def __str__(self) -> str:
+        if self.typ == Type.number:
+            return str(self.number)
+
+        if self.typ == Type.id:
+            return self.name
+
+        if self.typ == Type.error:
+            return str(self.typ)
+
+        return str(self.typ)
+
+
+def tokenize(scanner):
+    """
+    It is the generator that creates tokens from the scanner's output
+    :param scanner:
+    """
+    while scanner.has_next():
+        loc = scanner.location()
+        if scanner.expected_next(simple_tokens):
+            lexeme = scanner.move_next()
+            yield Token(loc, simple_tokens[lexeme])
+        elif scanner.expected_next(string.digits):
+            is_int = True
+
+            num = scanner.move_next()
+            while scanner.expected_next(string.digits):
+                num += scanner.move_next()
+
+            if scanner.expected_next('.'):
+                is_int = False
+                num += scanner.move_next()
+
+                while scanner.expected_next(string.digits):
+                    num += scanner.move_next()
+
+            if scanner.expected_next('Ee'):
+                is_int = False
+                num += scanner.move_next()
+                if scanner.expected_next('+-'):
+                    num += scanner.move_next()
+
+                if scanner.expected_next(string.digits):
+                    num += scanner.move_next()
+                    while scanner.expected_next(string.digits):
+                        num += scanner.move_next()
+
+                else:
+                    if scanner.has_next():
+                        yield Token(
+                            loc=scanner.location(),
+                            typ=Type.error,
+                            err=Error(loc=scanner.location(), expected_chars=['0-9'], parsed_chars=num,
+                                      next_char=scanner.look_next())
+                        )
+
+                        scanner.move_next()
+                    else:
+                        yield Token(
+                            loc=scanner.location(),
+                            typ=Type.error,
+                            err=Error(loc=scanner.location(), expected_chars=['0-9'], parsed_chars=num, next_char='')
+                        )
+
+                    continue
+
+            try:
+                yield Token(loc, Type.number, num=int(num) if is_int else float(num))
+            except ValueError:
+                yield Token(
+                    loc=loc,
+                    typ=Type.error,
+                    err=Error(loc=scanner.location(), expected_chars=['(0-9)+[.(0-9)*][[E|e][+|-](0-9)+]'],
+                              parsed_chars=num, next_char=scanner.look_next())
+                )
+
+        elif scanner.expected_next(string.ascii_lowercase):
+            lit = scanner.move_next()
+            while scanner.expected_next(string.ascii_lowercase):
+                lit += scanner.move_next()
+
+            yield Token(loc, Type.id, lit)
+        elif scanner.expected_next(' \t'):
+            scanner.move_next()
+        else:
+            yield Token(
+                loc=loc,
+                typ=Type.error,
+                err=Error(loc=scanner.location(), expected_chars='(0-9)|(a-z)|(|)|+|-|*|/|=', parsed_chars='',
+                          next_char=scanner.look_next())
+            )
+            scanner.move_next()
+
+    yield Token(scanner.location(), Type.eol)
+
+
+class Scanner:
+    """
+    Scanner provides the characters in the input stream. The scanner can look at one single character in ahead.
     """
     def __init__(self, source):
         self.src = source
         self.loc = -1
 
+    def location(self):
+        return self.loc + 1
+
     def look_next(self):
-        if self.loc + 1 >= len(self.src):
+        if not self.has_next():
             raise Exception("no input")
 
         return self.src[self.loc + 1]
@@ -21,254 +184,8 @@ class TokenReader:
         self.loc += 1
         return tok
 
+    def has_next(self):
+        return self.loc + 1 < len(self.src)
 
-"""
-The sets of tokens that each rule starts from.
-Helps in parsing and error handling.
-"""
-
-expr_paren_starts = {tokenizer.Type.l_paren}
-short_prod_starts = expr_paren_starts | {tokenizer.Type.number, tokenizer.Type.id}
-prod_starts = short_prod_starts
-sum_starts = prod_starts | {tokenizer.Type.add, tokenizer.Type.sub}
-expr_starts = sum_starts
-
-"""
-All the valid tokens (no error and no eol tokens)
-"""
-all_tokens = {
-    tokenizer.Type.l_paren, tokenizer.Type.r_paren,
-    tokenizer.Type.number, tokenizer.Type.id,
-    tokenizer.Type.add, tokenizer.Type.sub,
-    tokenizer.Type.mul, tokenizer.Type.div,
-    tokenizer.Type.equal
-}
-
-
-def parse(reader, errors):
-    """
-    Recognizes:
-    EE := E [= E]
-    :param reader:
-    :param errors:
-    :return: AST
-    """
-    tree = parse_expr(reader, errors)
-    if find_expected(reader, [tokenizer.Type.equal, tokenizer.Type.eol], errors):
-        if reader.look_next().typ == tokenizer.Type.equal:
-            reader.move_next()
-            tree2 = parse_expr(reader, errors)
-            find_expected(reader, [tokenizer.Type.eol], errors)
-
-            tree = ast.addition([tree, ast.negative(tree2)])
-
-    return tree
-
-
-def parse_expr(reader, errors):
-    """
-    Recognizes the rule:
-    EE := E [= E]
-
-    :param reader:
-    :param errors:
-    :return: AST
-    """
-    return parse_sum(reader, errors)
-
-
-def parse_sum(reader, errors):
-    """
-    Recognizes the rule:
-    S := ['+'|'-'] P ('+'|'-' P)*
-
-    :param reader:
-    :param errors:
-    :return: AST
-    """
-    if not find_expected(reader, sum_starts, errors):
-        return None
-
-    neg = reader.look_next().typ == tokenizer.Type.sub
-    if reader.look_next().typ in [tokenizer.Type.add, tokenizer.Type.sub]:
-        reader.move_next()
-
-    tree = parse_product(reader, errors)
-    if neg:
-        tree = ast.negative(tree)
-
-    operands = [tree]
-
-    if find_expected(reader, all_tokens | {tokenizer.Type.eol}, errors):
-        while reader.look_next().typ in [tokenizer.Type.add, tokenizer.Type.sub]:
-            neg = reader.look_next().typ == tokenizer.Type.sub
-            reader.move_next()
-            tree = parse_product(reader, errors)
-            if neg:
-                tree = ast.negative(tree)
-
-            operands.append(tree)
-
-    return ast.addition(operands)
-
-
-def parse_product(reader, errors):
-    """
-    Recognizes the rules:
-    P := num ('*'|'/' P)*
-    P := num (ID | F | PE) ('*'|'/' P)*
-    P := (ID | F | PE) ('*'|'/' P)*
-    F := 'log' SP
-    :param reader:
-    :param errors:
-    :return: AST
-    """
-    operation = None
-    operands = []
-    while True:
-        if not find_expected(reader, prod_starts, errors):
-            break
-
-        prev_tok = reader.look_next()
-        if prev_tok.typ == tokenizer.Type.id:
-            name = reader.move_next().name
-            if name == 'log':
-                tree = ast.logarithm(parse_short_product(reader, errors))
-            else:
-                tree = ast.variable(name)
-
-        elif prev_tok.typ == tokenizer.Type.number:
-            number = reader.move_next().number
-            tree = ast.number(number)
-        elif prev_tok.typ == tokenizer.Type.l_paren:
-            tree = parse_expr_in_parenthesis(reader, errors)
-        else:
-            assert False
-
-        if operation == ast.Type.mul:
-            operands.append(tree)
-        elif operation == ast.Type.inv:
-            operands.append(ast.inverse(tree))
-        else:
-            operands.append(tree)
-
-        if (prev_tok.typ == tokenizer.Type.number) and (operation in (None, ast.Type.mul)):
-            if not find_expected(reader, all_tokens - {tokenizer.Type.number} | {tokenizer.Type.eol}, errors):
-                break
-        else:
-            if not find_expected(
-                    reader,
-                    all_tokens - {tokenizer.Type.number, tokenizer.Type.id, tokenizer.Type.l_paren} | {tokenizer.Type.eol},
-                    errors
-            ):
-                break
-
-        tok = reader.look_next()
-        if tok.typ == tokenizer.Type.mul:
-            operation = ast.Type.mul
-            reader.move_next()
-        elif tok.typ == tokenizer.Type.div:
-            operation = ast.Type.inv
-            reader.move_next()
-        elif (prev_tok.typ == tokenizer.Type.number) and (
-                    tok.typ in [tokenizer.Type.id, tokenizer.Type.l_paren]) and (operation in [None, ast.Type.mul]):
-            operation = ast.Type.mul
-        else:
-            break
-
-    return ast.multiplication(operands)
-
-
-def parse_short_product(reader, errors):
-    """
-    Recognizes the rules:
-    SP := PE
-    SP := num
-    SP := num ID
-    SP := num F
-    SP := ID
-    SP := F
-    F := 'log' SP
-    :param reader:
-    :param errors:
-    :return: AST
-    """
-    if not find_expected(reader, prod_starts, errors):
-        return None
-
-    if reader.look_next().typ == tokenizer.Type.l_paren:
-        return parse_expr_in_parenthesis(reader, errors)
-
-    operands = []
-    if reader.look_next().typ == tokenizer.Type.number:
-        number = reader.move_next().number
-        operands.append(ast.number(number))
-
-    if reader.look_next().typ == tokenizer.Type.id:
-        name = reader.move_next().name
-        if name == 'log':
-            operands.append(ast.logarithm(parse_short_product(reader, errors)))
-        else:
-            operands.append(ast.variable(name))
-
-    return ast.multiplication(operands)
-
-
-def parse_expr_in_parenthesis(reader, errors):
-    """
-    Recognizes the rules:
-    E := '(' E ')'
-    :param reader:
-    :param errors:
-    :return: AST
-    """
-    if not find_expected(reader, expr_paren_starts, errors):
-        return None
-
-    reader.move_next()
-    tree = parse_expr(reader, errors)
-
-    if find_expected(reader, [tokenizer.Type.r_paren], errors):
-        reader.move_next()
-
-    return tree
-
-
-def find_expected(reader, expected_tokens, errors) -> bool:
-    if reader.look_next().typ not in expected_tokens:
-        errors.append(
-            Error(expected_tokens=expected_tokens, received_token=reader.look_next())
-        )
-        while (reader.look_next().typ != tokenizer.Type.eol) and reader.look_next().typ not in expected_tokens:
-            reader.move_next()
-
-    return reader.look_next().typ in expected_tokens
-
-
-class Error:
-    def __init__(self, expected_tokens, received_token):
-        self.location = received_token.location
-        self.expected_tokens = expected_tokens
-        self.received_token = received_token
-
-        exp_simple_toks = "','".join(str(t.value) for t in expected_tokens if t.value in tokenizer.simple_tokens)
-        exp_other_toks = ",".join(str(t.value) for t in expected_tokens if t.value not in tokenizer.simple_tokens)
-
-        exp_toks = f"'{exp_simple_toks}'" if exp_simple_toks else ""
-        if exp_other_toks:
-            if exp_toks:
-                exp_toks += ','
-
-            exp_toks += exp_other_toks
-
-        if received_token.typ == tokenizer.Type.error:
-            err = received_token.err
-            self.msg = f"error @ {self.location}: invalid char: '{err.next_char}', expected tokens: {exp_toks} (chars: {err.expected_chars})"
-        else:
-            self.msg = f"error @ {self.location}: unexpected token: {received_token.typ.value}, expected tokens: {exp_toks}"
-
-    def __str__(self) -> str:
-        return repr(self)
-
-    def __repr__(self) -> str:
-        return self.msg
+    def expected_next(self, expected_values):
+        return self.has_next() and (self.look_next() in expected_values)
