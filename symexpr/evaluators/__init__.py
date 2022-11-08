5abc1d891b47e480a9f9d02a7f5c45a93ef76738
Slava Chernoy <vchernoy@gmail.com>
2022-11-08 00:36:59 -0800

    add stringify

--- symexpr/evaluators/__init__.py
+++ symexpr/evaluators/__init__.py
@@ -3,6 +3,7 @@ from symexpr.evaluators.expand import expand
 from symexpr.evaluators.evalf import evalf
 from symexpr.evaluators.solve import solve
 from symexpr.evaluators.subs import subs, subse
+from symexpr.evaluators.stringify import stringify
 
 """
 A set of tools that allow to manipulate with AST
---
+++ symexpr/evaluators/stringify.py
@@ -0,0 +1,113 @@
+import functools
+import symexpr.ast as ast
+
+
+@functools.singledispatch
+def stringify(expr, in_parenthesis: bool = False) -> str:
+    """
+    :param in_parenthesis:
+    :param expr: AST
+    :return: AST
+    """
+    raise TypeError('cannot stringify {expr}, {in_parenthesis=}')
+
+
+@stringify.register
+def _add_stringify(self: ast.Add, in_parenthesis: bool = False) -> str:
+    res = ''
+    if self.coefficient == -1:
+        res = '-'
+    elif self.coefficient != 1:
+        res = str(self.coefficient)
+
+    if self.vars:
+        res += ast._vars_to_str(self) + '*'
+
+    extra_parenthesis = res != ''
+    if extra_parenthesis:
+        res += '('
+
+    res += '+'.join(stringify(n, True) for n in self.operands)
+    if extra_parenthesis:
+        res += ')'
+
+    return f'({res})' if in_parenthesis else res
+
+
+@stringify.register
+def _mul_stringify(self: ast.Mul, in_parenthesis: bool = False) -> str:
+    res = ''
+    if self.coefficient == -1:
+        res = '-'
+    elif self.coefficient != 1:
+        res = str(self.coefficient)
+
+    if self.vars:
+        res += ast._vars_to_str(self) + '*('
+
+    res += '*'.join(stringify(n, True) for n in self.operands)
+    if self.vars:
+        res += ')'
+
+    return f'({res})' if in_parenthesis else res
+
+
+@stringify.register
+def _inv_stringify(self: ast.Inv, in_parenthesis: bool = False) -> str:
+    if (self.coefficient == -1) and self.vars:
+        res = '-'
+    elif (self.coefficient != 1) or not self.vars:
+        res = str(self.coefficient)
+    else:
+        res = ''
+
+    res += ast._vars_to_str(self)
+    res += '/' + stringify(self.operands[0], True)
+
+    return f'({res})' if in_parenthesis else res
+
+
+@stringify.register
+def _one_stringify(self: ast.One, in_parenthesis: bool = False) -> str:
+    res = ''
+    if (self.coefficient == -1) and self.vars:
+        res = '-'
+    elif (self.coefficient != 1) or not self.vars:
+        res = str(self.coefficient)
+
+    res += ast._vars_to_str(self)
+    around_parenthesis = in_parenthesis and ((self.coefficient != 1 and self.vars) or (self.coefficient < 0))
+    return f'({res})' if around_parenthesis else res
+
+
+@stringify.register
+def _log_stringify(self: ast.Log, in_parenthesis: bool = False) -> str:
+    res = ''
+    if self.coefficient == -1:
+        res = '-'
+    elif self.coefficient != 1:
+        res = str(self.coefficient)
+
+    if self.vars:
+        res += ast._vars_to_str(self) + '*'
+
+    res += 'log ' + stringify(self.operands[0], True)
+
+    return f'({res})' if in_parenthesis else res
+
+
+@stringify.register
+def _exp_stringify(self:ast.Exp, in_parenthesis: bool = False) -> str:
+    res = ''
+    if self.coefficient == -1:
+        res = '-'
+    elif self.coefficient != 1:
+        res = str(self.coefficient)
+
+    if self.vars:
+        res += ast._vars_to_str(self) + '*'
+
+    res += 'exp ' + stringify(self.operands[0], True)
+
+    return f'({res})' if in_parenthesis else res
+
