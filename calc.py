69e4fcfb0e7e081fc89d142391c7fbdea9439288
Slava Chernoy <vchernoy@gmail.com>
2023-06-27 01:29:03 -0700

    refactor calc

--- calc.py
+++ calc.py
@@ -1,10 +1,10 @@
 import itertools
+import operator
 import random
 import symexpr.ast as ast
 import symexpr.tokenizer as tokenizer
 import symexpr.parser as parser
 import symexpr.evaluators as evaluators
-
 
 """
 The interactive symbolic calculator.
@@ -23,90 +23,102 @@ def all_ways_to_compute(expr: ast.Node) -> list[ast.Node]:
     ]
 
 
+def s_len(n: ast.Node) -> int:
+    return len(str(n))
+
+
+def print_errors(inp: str, errors: parser.Errors):
+    errors.sort(key=operator.attrgetter('loc'))
+    buf = [' '] * (len(inp) + 1)
+    for err in errors:
+        buf[err.loc] = '^'
+
+    print(inp)
+    print(''.join(buf))
+
+    for err in errors:
+        print(err)
+
+
+def check_solution(simplified: ast.Node, var: str, solution: ast.Node):
+    print('checking the solution...')
+    subs_ast = evaluators.subse(simplified, {var: solution})
+    # print 'substituted the solution to the expression: ', subs_ast
+    subs_attempts = all_ways_to_compute(subs_ast)
+    subs_attempts.sort(key=s_len)
+    simplified_subs = subs_attempts[0]
+    print(f'substitutes the solution to the original expression: {simplified_subs}')
+
+    if simplified_subs.numeric() and simplified_subs.coeff == 0:
+        print('correct, 0 is expected')
+    elif simplified_subs.numeric() and abs(simplified_subs.coeff) < 1e-10:
+        print('correct, close to 0 is expected')
+    elif ast.all_vars(simplified_subs):
+        free_vars = ast.all_vars(simplified_subs)
+        print(f'there are still free variables {free_vars}')
+        print("let's generate random assignments for them...")
+        assignment = {v: random.uniform(-1000, 1000) for v in free_vars}
+        print(f'the generated assignment to be substituted is {assignment}')
+        final_ast = evaluators.subs(simplified_subs, assignment)
+        final_attempts = all_ways_to_compute(final_ast)
+        final_attempts.sort(key=s_len)
+        final_subs = final_attempts[0]
+        print(f'random assignment gives {final_subs}')
+
+        if final_subs.numeric() and final_subs.coeff == 0:
+            print('correct, 0 is expected')
+        elif final_subs.numeric() and abs(final_subs.coeff) < 1e-10:
+            print('correct, close to 0 is expected')
+        else:
+            print('ups... something wrong happened, test it more!')
+
+    else:
+        print('ups... something wrong happened, test it more!')
+
+
 def main():
     n_line = 1
     while True:
         inp = input(f'input {n_line} > ')
-        toks = tokenizer.tokenize(tokenizer.Scanner(inp))
-        tok_list = list(toks)
-        if len(tok_list) == 1 and tok_list[0].typ == tokenizer.Type.eol:
+        token_list = list(tokenizer.tokenize(tokenizer.Scanner(inp)))
+        if len(token_list) == 1 and token_list[0].typ == tokenizer.Type.eol:
             break
 
-        tok_reader = parser.TokenReader(tok_list)
+        tok_reader = parser.TokenReader(token_list)
         errors = []
         expr = parser.parse(tok_reader, errors)
 
         if errors:
-            errors.sort(key=lambda e: e.loc)
-            buf = [' '] * (len(inp) + 1)
-            for err in errors:
-                buf[err.loc] = '^'
-
-            print(inp)
-            print(''.join(buf))
-
-            for err in errors:
-                print(err)
+            print_errors(inp, errors)
 
         if expr:
             print(f'parsed expression: {expr}')
             attempts = all_ways_to_compute(expr)
-            attempts.sort(key=lambda n: len(str(n)))
+            attempts.sort(key=s_len)
             simplified = attempts[0]
             print(f'simplified expression: {simplified}')
 
             variables = ast.all_vars(simplified)
             if variables:
                 print("equation on '" + "', '".join(variables) + "' is detected")
-                var = next(itertools.chain([v for v in 'xyzabc' if v in variables], variables))
+                var = next(itertools.chain((v for v in 'xyzabc' if v in variables), variables))
 
                 print(f'trying to solve equation on variable {var}')
                 solve_res = evaluators.solve(simplified, var)
                 if solve_res:
                     root, root_expr = solve_res
-                    sols = all_ways_to_compute(root) + [root]
-                    sols.sort(key=lambda n: len(str(n)))
-                    sol = sols[0]
-                    print(f'solution: {root_expr} = {sol}')
+                    solutions = all_ways_to_compute(root) + [root]
+                    solutions.sort(key=s_len)
+                    solution = solutions[0]
+                    print(f'solution: {root_expr} = {solution}')
 
-                    if not sol.numeric():
-                        evaluated_sol = evaluators.evalf(sol)
-                        if str(evaluated_sol) != str(sol):
-                            print(f'approximate solution: root_expr = {evaluators.evalf(sol)}')
+                    if not solution.numeric():
+                        evaluated_sol = evaluators.evalf(solution)
+                        if str(evaluated_sol) != str(solution):
+                            print(f'approximate solution: root_expr = {evaluators.evalf(solution)}')
 
                     if root_expr.degree() == 1:
-                        print('checking the solution...')
-                        subs_ast = evaluators.subse(simplified, {var: sol})
-                        # print 'substituted the solution to the expression: ', subs_ast
-                        subs_attempts = all_ways_to_compute(subs_ast)
-                        subs_attempts.sort(key=lambda n: len(str(n)))
-                        simplified_subs = subs_attempts[0]
-                        print(f'substitutes the solution to the original expression: {simplified_subs}')
-                        if simplified_subs.numeric() and simplified_subs.coeff == 0:
-                            print('correct, 0 is expected')
-                        elif simplified_subs.numeric() and abs(simplified_subs.coeff) < 1e-10:
-                            print('correct, close to 0 is expected')
-                        elif ast.all_vars(simplified_subs):
-                            free_vars = ast.all_vars(simplified_subs)
-                            print(f'there are still free variables {free_vars}')
-                            print("let's generate random assignments for them...")
-                            assignment = {v: random.uniform(-1000, 1000) for v in free_vars}
-                            print(f'the generated assignment to be substituted is {assignment}')
-                            final_ast = evaluators.subs(simplified_subs, assignment)
-                            final_attempts = all_ways_to_compute(final_ast)
-                            final_attempts.sort(key=lambda n: len(str(n)))
-                            final_subs = final_attempts[0]
-                            print(f'random assignment gives {final_subs}')
-                            if final_subs.numeric() and final_subs.coeff == 0:
-                                print('correct, 0 is expected')
-                            elif final_subs.numeric() and abs(final_subs.coeff) < 1e-10:
-                                print('correct, close to 0 is expected')
-                            else:
-                                print('ups... something wrong happened, test it more!')
-
-                        else:
-                            print('ups... something wrong happened, test it more!')
-
+                        check_solution(simplified, var, solution)
                 else:
                     print(f'cannot solve the equation over {var}')
 
