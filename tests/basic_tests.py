5119447658da9c26457b2b3f3edd509ca0506a40
Slava Chernoy <vchernoy@gmail.com>
2023-06-27 01:07:22 -0700

    cleanup tests

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -5,84 +5,69 @@ import unittest
 
 
 class TestCalc(unittest.TestCase):
+    _table: tuple[tuple[str, float], ...] = (
+        ('1.+2.+3.+4.+5.+6.+7.+8.+9.', 45.0),
+        ('-1.+1.' * 20, 0.),
+        ('-1.+2.' * 20, 20.0),
+        ('+(-1.)*(-1.)-2.*2.' * 20, -60.0),
+        ('1.' + '*(-1.)' * 21, -1.0),
+        ('-1.' + '*(-2.)' * 10, -1024.0),
+        ('*'.join(['(3.-1.)'] * 10), 1024.0),
+        ('10.-20.+30.-40.+50.-60.+70.-80.', -40.0),
+    )
+
+    def _test_int(self, inp: str, expected: int | None = None):
+        expected = str(int(inp)) if expected is None else expected
+        expr, errors = parse(inp)
+        self.assertEqual(errors, [])
+        self.assertEqual(int(str(evaluators.simplify(expr))), expected)
+        expr1 = evaluators.expand(expr)
+        self.assertEqual(int(str(evaluators.simplify(expr1))), expected)
+        expr2 = evaluators.evalf(expr)
+        self.assertEqual(int(str(expr2)), expected)
+
+    def _test_float(self, inp: str, expected: float | None = None):
+        expected = float(inp) if expected is None else expected
+        expr, errors = parse(inp)
+        self.assertEqual(errors, [])
+        self.assertEqual(float(str(evaluators.simplify(expr))), expected)
+        expr1 = evaluators.expand(expr)
+        self.assertEqual(float(str(evaluators.simplify(expr1))), expected)
+        expr2 = evaluators.evalf(expr)
+        self.assertEqual(float(str(expr2)), expected)
+
     def test_table(self):
-        table: tuple[str,...] = (
+        table: tuple[str, ...] = (
             '0', '000', '+0', '1', '-1', '1234', '-903341234', '13156545', '00013145353',
         )
         for inp in table:
-            expr, errors = parse(inp)
-            expected = str(int(inp))
-            self.assertEqual(errors, [])
-            self.assertEqual(str(evaluators.simplify(expr)), expected)
-            expr1 = evaluators.expand(expr)
-            self.assertEqual(str(evaluators.simplify(expr1)), expected)
-            expr2 = evaluators.evalf(expr)
-            self.assertEqual(str(expr2), expected)
+            self._test_int(inp)
 
     def test_int_arith(self):
-        table: tuple[tuple[str, str],...] = (
-            ('1+2+3+4+5+6+7+8+9', '45'),
-            ('-1+1' * 20, '0'),
-            ('-1+2' * 20, '20'),
-            ('+(-1)*(-1)-2*2' * 20, '-60'),
-            ('1' + '*(-1)' * 21, '-1'),
-            ('-1' + '*(-2)' * 10, '-1024'),
-            ('*'.join(['(3-1)'] * 10), '1024'),
-            ('10-20+30-40+50-60+70-80', '-40'),
+        table: tuple[tuple[str, int], ...] = (
+            ('1+2+3+4+5+6+7+8+9', 45),
+            ('-1+1' * 20, 0),
+            ('-1+2' * 20, 20),
+            ('+(-1)*(-1)-2*2' * 20, -60),
+            ('1' + '*(-1)' * 21, -1),
+            ('-1' + '*(-2)' * 10, -1024),
+            ('*'.join(['(3-1)'] * 10), 1024),
+            ('10-20+30-40+50-60+70-80', -40),
         )
         for inp, expected in table:
-            expr, errors = parse(inp)
-            self.assertEqual(errors, [])
-            self.assertEqual(str(evaluators.simplify(expr)), expected)
-            expr1 = evaluators.expand(expr)
-            self.assertEqual(str(evaluators.simplify(expr1)), expected)
-            expr2 = evaluators.evalf(expr)
-            self.assertEqual(str(expr2), expected)
+            self._test_int(inp, expected)
 
     def test_int_range(self):
         for n in range(2000):
             for inp in f'{n}', f'+{n}', f'-{n}':
-                expected = str(int(inp))
-                expr, errors = parse(inp)
-                self.assertEqual(errors, [])
-                self.assertEqual(str(evaluators.simplify(expr)), expected)
-                expr1 = evaluators.expand(expr)
-                self.assertEqual(str(evaluators.simplify(expr1)), expected)
-                expr2 = evaluators.evalf(expr)
-                self.assertEqual(str(expr2), expected)
+                self._test_int(inp)
 
     def test_float_arith(self):
-        table: tuple[tuple[str, float],...] = (
-            ('1.+2.+3.+4.+5.+6.+7.+8.+9.', 45.0),
-            ('-1.+1.' * 20, 0.),
-            ('-1.+2.' * 20, 20.0),
-            ('+(-1.)*(-1.)-2.*2.' * 20, -60.0),
-            ('1.' + '*(-1.)' * 21, -1.0),
-            ('-1.' + '*(-2.)' * 10, -1024.0),
-            ('*'.join(['(3.-1.)'] * 10), 1024.0),
-            ('10.-20.+30.-40.+50.-60.+70.-80.', -40.0),
-        )
-        for inp, expected in table:
-            expr, errors = parse(inp)
-            self.assertEqual(errors, [])
-            self.assertEqual(float(str(evaluators.simplify(expr))), expected)
-            expr1 = evaluators.expand(expr)
-            self.assertEqual(float(str(evaluators.simplify(expr1))), expected)
-            expr2 = evaluators.evalf(expr)
-            self.assertEqual(float(str(expr2)), expected)
+        for inp, expected in TestCalc._table:
+            self._test_float(inp, expected)
 
     def test_numeric_arith(self):
-        table: tuple[tuple[str, float],...] = (
-            ('1.+2.+3.+4.+5.+6.+7.+8.+9.', 45.0),
-            ('-1.+1.' * 20, 0.),
-            ('-1.+2.' * 20, 20.0),
-            ('+(-1.)*(-1.)-2.*2.' * 20, -60.0),
-            ('1.' + '*(-1.)' * 21, -1.0),
-            ('-1.' + '*(-2.)' * 10, -1024.0),
-            ('*'.join(['(3.-1.)'] * 10), 1024.0),
-            ('10.-20.+30.-40.+50.-60.+70.-80.', -40.0),
-        )
-        for inp, expected in table:
+        for inp, expected in TestCalc._table:
             expr, errors = parse(inp)
             self.assertEqual(errors, [])
             self.assertEqual(float(str(evaluators.evalf(expr))), expected)
@@ -110,27 +95,13 @@ class TestCalc(unittest.TestCase):
         for m in range(2000):
             n = m / 5
             for inp in f'{n}', f'+{n}', f'-{n}':
-                expected = float(inp)
-                expr, errors = parse(inp)
-                self.assertEqual(errors, [])
-                self.assertEqual(float(str(evaluators.simplify(expr))), expected)
-                expr1 = evaluators.expand(expr)
-                self.assertEqual(float(str(evaluators.simplify(expr1))), expected)
-                expr2 = evaluators.evalf(expr)
-                self.assertEqual(float(str(expr2)), expected)
+                self._test_float(inp)
 
     def test_float_e_range(self):
         for m in range(2000):
             n = m / 5
             for inp in f'{n}' + 'E-20', f'+{n}e+10', f'-{n}e15':
-                expected = float(inp)
-                expr, errors = parse(inp)
-                self.assertEqual(errors, [])
-                self.assertEqual(float(str(evaluators.simplify(expr))), expected)
-                expr1 = evaluators.expand(expr)
-                self.assertEqual(float(str(evaluators.simplify(expr1))), expected)
-                expr2 = evaluators.evalf(expr)
-                self.assertEqual(float(str(expr2)), expected)
+                self._test_float(inp)
 
     def test_binomial_num(self):
         for n in 10, 100, 1000:
@@ -150,8 +121,13 @@ class TestCalc(unittest.TestCase):
         for n in 1, 10, 20, 100, 101:
             inp = ' * '.join(['(a+b) * (a-b)'] * n)
             expr, errors = parse(inp)
-            exprs = [expr, evaluators.simplify(expr), evaluators.expand(expr), evaluators.expand(evaluators.simplify(expr))]
-            for assignment in {'a': 2, 'b': 1}, :
+            exprs = [
+                expr,
+                evaluators.simplify(expr),
+                evaluators.expand(expr),
+                evaluators.expand(evaluators.simplify(expr)),
+            ]
+            for assignment in {'a': 2, 'b': 1}, {'a': 3, 'b': -2}:
                 expected = (assignment['a'] ** 2 - assignment['b'] ** 2) ** n
                 for expr in exprs:
                     expr0 = evaluators.subs(expr, assignment)
